02bd8efaafdb3382d5b9cc2b8198bab537402abc
Aayush014 <aayushpatel1411@gmail.com>
2023-09-07 21:57:14 +0530

    7-9-2023 1

--- lib/CartScreen.dart
+++ lib/CartScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -39,8 +40,8 @@ class _CartScreenState extends State<CartScreen> {
                   boxShadow: [
                     BoxShadow(
                         color: Colors.grey.shade400,
-                        blurRadius: 1,
-                        spreadRadius: 0.5),
+                        blurRadius: 0.5,
+                        spreadRadius: 0.1),
                   ],
                 ),
                 child: Row(
@@ -67,17 +68,20 @@ class _CartScreenState extends State<CartScreen> {
                     Column(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Text("Bacon Burger"),
+                        const Text("Bacon Burger"),
                         Row(
                           children: [
+                            const SizedBox(
+                              width: 10,
+                            ),
                             InkWell(
                               child: Container(
                                 height: 25,
                                 width: 25,
-                                decoration: BoxDecoration(
+                                decoration: const BoxDecoration(
                                     color: Colors.amberAccent,
                                     shape: BoxShape.circle),
-                                child: Icon(Icons.add, size: 19),
+                                child: const Icon(Icons.add, size: 19),
                               ),
                               onTap: () {
                                 setState(() {
@@ -85,25 +89,27 @@ class _CartScreenState extends State<CartScreen> {
                                 });
                               },
                             ),
-                            SizedBox(
+                            const SizedBox(
                               width: 10,
                             ),
                             Text("$num"),
-                            SizedBox(
+                            const SizedBox(
                               width: 10,
                             ),
                             InkWell(
                               child: Container(
                                 height: 25,
                                 width: 25,
-                                decoration: BoxDecoration(
+                                decoration: const BoxDecoration(
                                     color: Colors.amberAccent,
                                     shape: BoxShape.circle),
-                                child: Icon(Icons.remove, size: 19),
+                                child: const Icon(Icons.remove, size: 19),
                               ),
                               onTap: () {
                                 setState(() {
-                                  num--;
+                                  if (num > 0) {
+                                    num--;
+                                  }
                                 });
                               },
                             ),
@@ -111,8 +117,160 @@ class _CartScreenState extends State<CartScreen> {
                         ),
                       ],
                     ),
-                    SizedBox(width: 50,),
-                    Text('250₹'),
+                    const SizedBox(
+                      width: 50,
+                    ),
+                    const Text('250₹'),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 13,
+              ),
+              Container(
+                height: 100,
+                width: 320,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.shade400,
+                        blurRadius: 0.5,
+                        spreadRadius: 0.1),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 15,
+                      height: 50,
+                    ),
+                    Container(
+                      height: 70,
+                      width: 105,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                            image: AssetImage('assets/images/b6_img.png'),
+                            fit: BoxFit.fill),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 15,
+                      height: 50,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text("Cheddar Burger"),
+                        Row(
+                          children: [
+                            InkWell(
+                              child: Container(
+                                height: 25,
+                                width: 25,
+                                decoration: const BoxDecoration(
+                                    color: Colors.amberAccent,
+                                    shape: BoxShape.circle),
+                                child: const Icon(Icons.add, size: 19),
+                              ),
+                              onTap: () {
+                                setState(() {
+                                  num1++;
+                                });
+                              },
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text("$num1"),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            InkWell(
+                              child: Container(
+                                height: 25,
+                                width: 25,
+                                decoration: const BoxDecoration(
+                                    color: Colors.amberAccent,
+                                    shape: BoxShape.circle),
+                                child: const Icon(Icons.remove, size: 19),
+                              ),
+                              onTap: () {
+                                setState(() {
+                                  if (num1 > 0) {
+                                    num1--;
+                                  }
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 40,
+                    ),
+                    const Text('380₹'),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Container(
+                height: 60,
+                width: 280,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 60,
+                      decoration: const BoxDecoration(color: Colors.black),
+                      child: const Image(
+                        image: AssetImage('assets/images/coupon.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Text(
+                      "Apply Coupons",
+                      style: GoogleFonts.poppins(
+                          fontSize: 17, color: Colors.white),
+                    ),
+                    const Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 35,
+              ),
+              Container(
+                width: 330,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Row(mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(height: 60,width: 10,),
+                        Text("Becon Burger",style: GoogleFonts.poppins(fontSize: 15),),
+                        Spacer(),
+                        Text("250₹")
+                      ],
+                    ),
                   ],
                 ),
               ),
@@ -125,3 +283,4 @@ class _CartScreenState extends State<CartScreen> {
 }
 
 int num = 1;
+int num1 = 1;
